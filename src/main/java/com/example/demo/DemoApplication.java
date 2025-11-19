0858780845dae84003617c1d404cf16dd1820c88
Stelios Kaltsounis <stelios.kaltsounis@gmail.com>
2025-11-19 13:38:43 +0200

    vs on line third

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World! This is my first attempt to use CI/CD. corrected";
+		return "Hello World! This is my first attempt to use CI/CD. corrected. Third";
 	}
 }
